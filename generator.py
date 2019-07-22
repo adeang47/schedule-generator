8c3adc77fdfa525cbc523a420f56a5f24c597d70
Andrew DeAngelis <andrewdeangelis@Andrew-DeAngelis-MacBook-Pro.local>
2019-07-22 11:20:16 -0400

    add multi week scheduling

--- generator.py
+++ generator.py
@@ -6,7 +6,6 @@ from datetime import date, timedelta
 import numpy as np
 import pandas as pd
 
-
 REPOS = {
     'Site API - Algo',
     'Site API - Interface',
@@ -19,7 +18,11 @@ REPOS = {
     'Answer Predictor',
     'Buoy Deployment',
     'Med Tools',
-    'Kube Charts'
+    'BuoyBazaar',
+    'buoy-shared-jenkins',
+    'buoy-terraform',
+    'buoy-flagr',
+    'buoy-helm-charts',
 }
 
 MAX_VAL = 5
@@ -172,12 +175,13 @@ def total_dev_pairing_difficulty(dev_matches):
     return dev_difficulty
 
 
-def pair_devs(dev_matches, dev_difficulty):
+def pair_devs(dev_matches, dev_difficulty, previous_pairs={}):
     """ pair two devs starting with the most difficult to pair to the least difficult
 
     Args:
         dev_matches(dict): dictionary of developer names with an ordered list of pairings by diff
         dev_difficulty(list): list sorted by devs that are most to least difficult to pair
+        previous_pairs(dict): dictionary with the key being the dev name and the value being their pair
 
     Returns:
         (list): list of tuples for the dev pairs
@@ -189,6 +193,8 @@ def pair_devs(dev_matches, dev_difficulty):
         if dev['name'] not in used_devs:
             for potential_dev in dev_matches[dev['name']]:
                 if potential_dev['name'] not in used_devs:
+                    if dev['name'] in previous_pairs and potential_dev['name'] in previous_pairs[dev['name']]:
+                        continue
                     dev_pairs.append((dev['name'], potential_dev['name']))
                     used_devs.extend([dev['name'], potential_dev['name']])
                     break
@@ -196,12 +202,13 @@ def pair_devs(dev_matches, dev_difficulty):
     return dev_pairs
 
 
-def build_pairs(devs):
+def build_pairs(devs, previous_pairs):
     """ build a schedule from a list of devs and their repo knowledge
 
     Args:
         devs(list): list of developer dictionaries containing the dev name,
             and dict of repos with the repo name and knowledge level
+        previous_pairs(dict): dictionary with the key being the dev name and the value being their pair
 
     Returns:
         (list): list of tuples for the dev pairs
@@ -210,7 +217,7 @@ def build_pairs(devs):
     ideal_dev_pairs = get_ideal_dev_pairs(devs)
     dev_matches = get_dev_matches(devs, ideal_dev_pairs)
     dev_difficulty = total_dev_pairing_difficulty(dev_matches)
-    dev_pairs = pair_devs(dev_matches, dev_difficulty)
+    dev_pairs = pair_devs(dev_matches, dev_difficulty, previous_pairs)
 
     return dev_pairs
 
@@ -275,7 +282,7 @@ def get_schedule_pair_idx(pairs, schedule_restrictions, schedule_date):
     return rand_pair_idx
 
 
-def build_schedule_order(dev_pairs, schedule_restrictions, start_date=date.today()):
+def build_schedule_order(dev_pairs, schedule_restrictions, start_date=date.today(), days=None):
     """ build the order of the schedule from the input dev pairs
         restrictions will prevent a dev pairing from being scheduled on their restricted dates
         the schedule will start at the input start date
@@ -291,9 +298,8 @@ def build_schedule_order(dev_pairs, schedule_restrictions, start_date=date.today
 
     """
     # 0 Monday - 6 Sunday
-    start_day = start_date.weekday()
     pairs = copy(dev_pairs)
-    days = len(dev_pairs)
+    days = len(dev_pairs) if days is None else days
     schedule = []
     # if the entered date is a weekend skip to Monday
     start_date = skip_weekend(start_date)
@@ -310,9 +316,11 @@ def build_schedule_order(dev_pairs, schedule_restrictions, start_date=date.today
     return schedule
 
 
-def load_devs():
+def load_devs(dev_ooo):
     """ load developers and their repository knowledge from the repo_knowledge csv file
 
+    Args:
+        dev_ooo(set): do not include devs that will be out of office for the schedule time
 
     Returns:
         (list): list dicts for each developer with their name,
@@ -322,18 +330,58 @@ def load_devs():
     devs = []
     df = pd.DataFrame.from_csv('repo_knowledge.csv')
     for row in df.iterrows():
-        dev = {'name': row[1]['Username'], 'repos': {}}
-        for repo in REPOS:
-            dev['repos'][repo] = row[1][
-                'Rate your knowledge/familiarity of the following repositories. [{}]'.format(repo)]
-        devs.append(dev)
+        if row[1]['Username'] not in dev_ooo:
+            dev = {'name': row[1]['Username'], 'repos': {}}
+            for repo in REPOS:
+                dev['repos'][repo] = row[1][
+                    'Rate your knowledge/familiarity of the following repositories. [{}]'.format(repo)]
+            devs.append(dev)
 
     return devs
 
 
-def build_schedule(start_date=date.today()):
-    devs = load_devs()
-    pairs = build_pairs(devs)
+def build_previous_pairs_from_schedule(schedule):
+    previous_pairs = {}
+    for schedule_date in schedule:
+        pair = schedule_date['pair']
+        previous_pairs[pair[0]] = [pair[1]]
+        previous_pairs[pair[1]] = [pair[0]]
+
+    return previous_pairs
+
+
+def print_schedule(schedule):
+    for schedule_date in schedule:
+        pair = schedule_date['pair']
+        print(f'{schedule_date["date"]} - {pair[0]} & {pair[1]}')
+
+
+def build_schedule(start_date=date.today(), days=5, previous_pairs=None):
+    dev_ooo = set()
+    if not previous_pairs:
+        previous_pairs = {
+            'adeangelis@buoyhealth.com': [],
+            'andrew.dumit@buoyhealth.com': [],
+            'jacob@buoyhealth.com': [],
+            'eddie@buoyhealth.com': [],
+            'mijail@buoyhealth.com': [],
+            'jimmy@buoyhealth.com': [],
+            'julia@buoyhealth.com': [],
+            'brett@buoyhealth.com': [],
+            'harry@buoyhealth.com': [],
+            'kunhua@buoyhealth.com': [],
+            'nick@buoyhealth.com': [],
+        }
+    devs = load_devs(dev_ooo)
+    pairs = build_pairs(devs, previous_pairs)
     schedule = build_schedule_order(pairs, {}, start_date)
 
     return schedule
+
+
+def build_multi_week_schedule(week_start=date.today(), weeks=2, previous_pairs=None):
+    for i in range(weeks):
+        schedule = build_schedule(start_date=week_start, days=5, previous_pairs=previous_pairs)
+        print_schedule(schedule)
+        previous_pairs = build_previous_pairs_from_schedule(schedule)
+        week_start = week_start + timedelta(days=7)
